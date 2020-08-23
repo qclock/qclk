046b42f69ba403f631d43e184777a592f5457ad1
Szabolcs Szabolcsi-Tóth <szabolcsit@hu.ibm.com>
2020-08-23 20:09:05 +0200

    navi

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -10,7 +10,7 @@ const Navigation = ({ state }) => (
   <div className={ clsx(css.navigation, css[state]) }>
 
     <svg width="412" height="276" viewBox="0 0 412 276" fill="none" xmlns="http://www.w3.org/2000/svg" className={ css.background }>
-      <circle cx="206" cy="448" r="448" />
+      <circle cx="206" cy="138" r="448" />
     </svg>
 
   </div>
