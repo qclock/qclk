9d617748ab40c2ed39d763dad2bc1e374c5e95f4
Szabolcs Szabolcsi-Tóth <szabolcsit@hu.ibm.com>
2020-08-24 18:21:09 +0200

    nav

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -7,12 +7,18 @@ import { Link, useStaticQuery, graphql } from "gatsby"
 import css from './navigation.module.scss'
 
 const Navigation = ({ state }) => (
-  <div className={ clsx(css.navigation, css[state]) }>
-
-    <svg width="412" height="276" viewBox="0 0 412 276" fill="none" xmlns="http://www.w3.org/2000/svg" className={ css.background }>
+  <div className={ clsx(css.navigationPanel, css[state]) }>
+    <nav className={ css.nav }>
+      <ul className={ css.menuitems }>
+        <li className={ css.menuitem }>Design</li>
+        <li className={ css.menuitem }>Time</li>
+        <li className={ css.menuitem }>Night Mode</li>
+        <li className={ clsx(css.menuitem, css.menu_settings) }>Settings</li>
+      </ul>
+    </nav>
+    <svg className={ css.background } viewBox="0 0 412 276" fill="none" xmlns="http://www.w3.org/2000/svg">
       <circle cx="206" cy="138" r="448" />
     </svg>
-
   </div>
 )
 
