6cd64e50df0ab85414e85ad5c032fbc0a8628575
Szabolcs Szabolcsi-Toth <szabolcsit@hu.ibm.com>
2020-12-23 10:38:21 +0100

    ws connectd

--- src/pages/nightmode.js
+++ src/pages/nightmode.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import useAppState from '../state'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Dimmer from '../components/Dimmer'
 
 const NightMode = () => {
-
+  const [ state, dispatch ] = useAppState();
   return (
     <Layout connected={ state.connected }>
       <SEO title="QCLK" />
